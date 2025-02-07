6b848ac432ec45b443b751da6ed76642f712b4b0
Srishti Goyal <24csu206@ncuindia.edu>
2025-02-07 16:18:36 +0530

    QUES5rotatematrix90deg.cpp

--- QUES5rotatematrix90deg.cpp
+++ QUES5rotatematrix90deg.cpp
@@ -5,7 +5,6 @@ int main() {
     int size = 3; // Size of the matrix (3x3)
     int matrix[3][3];
     
-    // Input the matrix
     cout << "Enter the elements of 3x3 matrix:" << endl;
     for(int i = 0; i < size; i++) {
         for(int j = 0; j < size; j++) {
@@ -13,8 +12,7 @@ int main() {
             cin >> matrix[i][j];
         }
     }
-    
-    // Print original matrix
+   
     cout << "\nOriginal Matrix:" << endl;
     for(int i = 0; i < size; i++) {
         for(int j = 0; j < size; j++) {
@@ -22,19 +20,15 @@ int main() {
         }
         cout << endl;
     }
-    
-    // Create a new matrix for rotated result
+   
     int rotated[3][3];
-    
-    // Rotate the matrix
-    // New position: row becomes col, col becomes (size-1-row)
+   
     for(int i = 0; i < size; i++) {
         for(int j = 0; j < size; j++) {
             rotated[j][size-1-i] = matrix[i][j];
         }
     }
     
-    // Print rotated matrix
     cout << "\nMatrix after 90 degree rotation:" << endl;
     for(int i = 0; i < size; i++) {
         for(int j = 0; j < size; j++) {
